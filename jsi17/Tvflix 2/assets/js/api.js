7d38b215c45370964708b3392cafe52ef3b9cd46
Quan190806ppp <105550611+Quan190806ppp@users.noreply.github.com>
2023-05-19 18:33:43 +0700

    Add files via upload

---
+++ jsi17/Tvflix 2/assets/js/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const api_key = '82484d01ee412ed105a9c5087074e593';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+/**
+ * fetch data from a server using the `url` and passes
+ * the result in JSON data to a `callback` function,
+ * along with an optional parameter if has `optionalParam`.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
